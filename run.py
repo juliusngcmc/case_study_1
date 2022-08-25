a9873c9cff6473c22675bc8240f73e42af89512a
yuthefirst <khanhduy9cnct@Gmail.com>
2022-08-25 17:36:27 +0700

    init commit

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from db import stock_eod_price
+from db import sensor_eod_value
 from marketstack import get
 
 if __name__ == "__main__":
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     for ticker in ticker_list:
         eod_data = get(ticker)
-        stock_eod_price(
+        sensor_eod_value(
             stock_ticker=eod_data['symbol'],
             eod_date=eod_data['date'],
             open_price=eod_data['open'],
